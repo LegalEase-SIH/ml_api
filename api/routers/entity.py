76a3be301fd3bf2e04fbde00a675ffe1e55291a7
aneesh-aparajit <aneeshaparajit.g2002@gmail.com>
2023-09-23 03:39:44 +0530

    init commit

---
+++ api/routers/entity.py
@@ -0,0 +1,33 @@
+from fastapi import APIRouter
+from collections import defaultdict
+from typing import Dict, List
+from pydantic import BaseModel
+import spacy
+import os
+
+# os.system(command="!pip install https://huggingface.co/opennyaiorg/en_legal_ner_trf/resolve/main/en_legal_ner_trf-any-py3-none-any.whl")
+
+nlp = spacy.load("en_legal_ner_trf")
+
+router = APIRouter()
+
+
+class EntityRequest(BaseModel):
+    raw_text: str
+
+
+class EntityResponse(BaseModel):
+    entitities: Dict[str, List[str]]
+
+
+@router.post("/", response_model=EntityResponse)
+def get_entities(request: EntityRequest):
+    raw_text = request.raw_text
+    doc = nlp(raw_text)
+
+    res = defaultdict(list)
+
+    for ent in doc.ents:
+        res[ent].append(ent.label_)
+
+    return EntityResponse(entitities=res)
